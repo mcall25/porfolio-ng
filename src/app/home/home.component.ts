d0b74674f8548e87e5c3c610a619f22ca95ce894
Michael Call <callmichael.bus@gmail.com>
2018-01-19 09:55:06 -0700

    portf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,28 +13,38 @@ import {trigger, state, style, transition, animate, keyframes, group} from '@ang
     ]),
     trigger('main-pic-small', [
       state('start', style({transform: 'translateX(0px)', opacity: '1'})),
-      state('end', style({transform: 'translateX(-1000px)',  opacity: '0'})),
+      state('end', style({transform: 'translateX(-1000px)',  opacity: '0',  display: 'none'})),
       transition('start <=> end', animate('0.3s 0.1s ease'))
     ]),
     trigger('about-me-small', [
-      state('start', style({transform: 'translateX(1000px)', opacity: '0'})),
+      state('start', style({transform: 'translateX(1000px)', opacity: '0', display: 'none'})),
       state('end', style({transform: 'translateX(0px)' })),
       transition('start <=> end', animate('0.3s 0.1s ease'))
     ]),
     trigger('portfolio-small', [
-      state('start', style({transform: 'translateX(1000px)'})),
+      state('start', style({transform: 'translateX(1000px)', opacity: '0', display: 'none'})),
       state('end', style({transform: 'translateX(0px)'})),
       transition('start <=> end', animate('0.3s 0.1s ease'))
     ]),
     trigger('contact-small', [
-      state('start', style({transform: 'translateX(1000px)'})),
+      state('start', style({transform: 'translateX(1000px)', opacity: '0', display: 'none'})),
       state('end', style({transform: 'translateX(0px)'})),
       transition('start <=> end', animate('0.3s 0.1s ease'))
     ]),
-    trigger('top-left', [
-      state('picture', style({transform: 'translateX(0px)', opacity: '0'})),
-      state('menu', style({transform: 'translateX(-300px)', 'background-color': 'white'})),
-      transition('picture <=> menu', animate('0.8s 0.0s ease'))
+    trigger('nav-bar', [
+      state('start', style({transform: 'translateX(0px)', opacity: '0'})),
+      state('end', style({transform: 'translateX(-300px)', 'background-color': 'white'})),
+      transition('start <=> end', animate('0.8s 0.0s ease'))
+    ]),
+    trigger('about-me', [
+      state('start', style({transform: 'translateX(1000px)', opacity: '0', display: 'none'})),
+      state('end', style({transform: 'translateX(0px)'})),
+      transition('start <=> end', animate('0.3s 0.1s ease'))
+    ]),
+    trigger('experience', [
+      state('start', style({transform: 'translateX(1000px)', opacity: '0', display: 'none'})),
+      state('end', style({transform: 'translateX(0px)'})),
+      transition('start <=> end', animate('0.3s 0.1s ease'))
     ]),
   ]
 })
@@ -46,14 +56,16 @@ export class HomeComponent implements OnInit {
   onSelectAboutMe:any
 
   // ANIMATIONS FOR SMALL VIEW
-  mainPicSmall= 'start';
-  aboutMeSmall= 'start';
-  portfolioSmall= 'start';
-  contactSmall= 'start';
+  mainPicSmall = 'start';
+  aboutMeSmall = 'start';
+  portfolioSmall = 'start';
+  contactSmall = 'start';
+  aboutMe = 'start';
+  experience = 'start';
 
   // ANIMATIONS FOR LARGE VIEW
-  mainPic= 'start';
-  topLeft = 'picture';
+  mainPic = 'start';
+  navBar = 'start';
 
 
   ngOnInit() {
@@ -114,13 +126,61 @@ export class HomeComponent implements OnInit {
 
   // BLUR THE NAV BAR
   closeNavMenu() {
-    if ( this.topLeft == 'menu') {
-      this.topLeft = 'picture'
-      this.mainPic = 'start'
-    }
-    else {
-      this.topLeft = 'menu'
+    // if ( this.topLeft == 'menu') {
+    //   this.topLeft = 'picture'
+    //   this.mainPic = 'start'
+    // }
+    // else {
+    //   this.topLeft = 'menu'
+    //   this.mainPic = 'end'
+    // }
+
+    if (this.mainPic == 'start') {
       this.mainPic = 'end'
+      this.navBar = 'menu'
+      this.aboutMe = 'end';
+    }
+    else if (this.aboutMe == 'end') {
+      this.mainPic = 'end'
+      this.navBar = 'menu'
+      this.aboutMe = 'start';
+      this.experience = 'end';
+
+    }
+
+  }
+
+
+  animateLarge(value) {
+
+    if (value == 'go-to-menu' &&  this.mainPic == 'start') {
+      this.mainPic = 'end'
+      this.navBar = 'end'
+      this.aboutMe = 'end';
+    }
+    else if (value == 'all-home-or-open' && this.mainPic == 'end') {
+      this.mainPic = 'start'
+      this.navBar = 'start'
+      this.aboutMe = 'start';
+    }
+    else if (value == 'all-home-or-open' && this.mainPic == 'start') {
+      this.mainPic = 'end'
+      this.navBar = 'end'
+      this.aboutMe = 'end';
+    }
+
+  }
+
+  cardAnimate(value) {
+    console.log('test func')
+    if (value == 'switch-card-about-me' &&  this.aboutMe == 'start') {
+      this.aboutMe = 'end';
+      this.experience = 'start'
+    }
+    else if (value == 'switch-card-experience' &&  this.experience == 'start') {
+      console.log('test')
+      this.experience = 'end'
+      this.aboutMe = 'start';
     }
   }
 
